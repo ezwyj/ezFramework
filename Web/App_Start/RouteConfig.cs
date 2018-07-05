0d47423d79ceed6efb7fa094892994c68c36c31a
2480446748@qq.com <2480446748@qq.com>
2018-07-05 10:54:23 +0800

    同步

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace VueWebTemplate.Web
+namespace Web
 {
     public class RouteConfig
     {
@@ -14,13 +14,9 @@ namespace VueWebTemplate.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Api",
-                url: "Api/{controller}/{action}"
-            );
-            routes.MapRoute(
-                name: "Client",
-                url: "{*pathInfo}",
-                defaults: new { controller = "Client", action = "Index" }
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- Web/Global.asax
+++ Web/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VueWebTemplate.Web.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web.WebApiApplication" Language="C#" %>
